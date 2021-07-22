bbc57fb22e68c93272c305bb38b1544921368c9a
Pragmatic Fox <ramon.witschi@alumni.ethz.ch>
2021-07-22 19:28:45 +0200

    update

--- experiments_script.py
+++ experiments_script.py
@@ -7,16 +7,47 @@ benchmark_path = pathlib.Path(pathlib.Path(__file__).parent.resolve(), "benchmar
 glob_loc_list = [
     {
         "exp_SAMPLING_MODE": 0,
-        "exp_N_SAMPLES_TRAIN": 1000,
-        "exp_IS_MODE_2_ABLATION": False,
         "exp_IS_TWOLINKAGE_CONSTRAINED": False,
+        "exp_IS_MODE_2_ABLATION": False,
+        "exp_N_SAMPLES_TRAIN": 1000,
         "exp_N_ITERATIONS": 10000
     },
-
-
-
+    {
+        "exp_SAMPLING_MODE": 0,
+        "exp_IS_TWOLINKAGE_CONSTRAINED": True,
+        "exp_IS_MODE_2_ABLATION": False,
+        "exp_N_SAMPLES_TRAIN": 1000,
+        "exp_N_ITERATIONS": 10000
+    },
+    {
+        "exp_SAMPLING_MODE": 1,
+        "exp_IS_TWOLINKAGE_CONSTRAINED": False,
+        "exp_IS_MODE_2_ABLATION": False,
+        "exp_N_SAMPLES_TRAIN": 1000,
+        "exp_N_ITERATIONS": 10000
+    },
+    {
+        "exp_SAMPLING_MODE": 1,
+        "exp_IS_TWOLINKAGE_CONSTRAINED": True,
+        "exp_IS_MODE_2_ABLATION": False,
+        "exp_N_SAMPLES_TRAIN": 1000,
+        "exp_N_ITERATIONS": 10000
+    },
+    {
+        "exp_SAMPLING_MODE": 2,
+        "exp_IS_TWOLINKAGE_CONSTRAINED": False,
+        "exp_IS_MODE_2_ABLATION": False,
+        "exp_N_SAMPLES_TRAIN": 1000,
+        "exp_N_ITERATIONS": 10000
+    },
+    {
+        "exp_SAMPLING_MODE": 2,
+        "exp_IS_TWOLINKAGE_CONSTRAINED": True,
+        "exp_IS_MODE_2_ABLATION": False,
+        "exp_N_SAMPLES_TRAIN": 1000,
+        "exp_N_ITERATIONS": 10000
+    },
 ]
-
 
 for exp in glob_loc_list :
     with open(benchmark_path) as f:
