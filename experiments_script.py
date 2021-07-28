bb7df41e5061788a5d7f196b49b5b8d63f525397
Pragmatic Fox <ramon.witschi@alumni.ethz.ch>
2021-07-28 21:08:09 +0200

    update

--- experiments_script.py
+++ experiments_script.py
@@ -5,6 +5,35 @@ import pathlib
 benchmark_path = pathlib.Path(pathlib.Path(__file__).parent.resolve(), "benchmark.py")
 compute_hist_grom_model_path = pathlib.Path(pathlib.Path(__file__).parent.resolve(), "compute_hist_from_model.py")
 
+
+'''
+# Experiment: The RNG Matters
+
+local_globals_list = [
+    {
+        "exp_SAMPLING_MODE": 0,
+    },
+    {
+        "exp_SAMPLING_MODE": 1,
+    },
+    {
+        "exp_SAMPLING_MODE": 2,
+    }
+]
+
+for local_globals in local_globals_list :
+    with open(benchmark_path) as f:
+        code = compile(f.read(), benchmark_path, "exec")
+        globals().update(local_globals)
+        globals()["__file__"] = benchmark_path
+        globals()["exp_N_SAMPLES_TRAIN"] = 1000
+        globals()["exp_N_ITERATIONS"] = 10000
+        globals()["exp_IS_MODE_2_ABLATION"] = False
+        globals()["exp_IS_TWOLINKAGE_CONSTRAINED"] = False
+        exec(code, globals())
+
+'''
+
 #'''
 
 # Experiment: Two-Linkage Main Experiments
@@ -65,7 +94,7 @@ for local_globals in local_globals_list :
         code = compile(f.read(), benchmark_path, "exec")
         globals().update(local_globals)
         globals()["__file__"] = benchmark_path
-        globals()["exp_N_ITERATIONS"] = 10000
+        globals()["exp_N_ITERATIONS"] = 20000
         globals()["exp_IS_MODE_2_ABLATION"] = False
         globals()["exp_IS_TWOLINKAGE_CONSTRAINED"] = False
         exec(code, globals())
@@ -304,33 +333,6 @@ for local_globals in local_globals_list :
 
 '''
 
-'''
-# Experiment: The RNG Matters
-
-local_globals_list = [
-    {
-        "exp_SAMPLING_MODE": 0,
-    },
-    {
-        "exp_SAMPLING_MODE": 1,
-    },
-    {
-        "exp_SAMPLING_MODE": 2,
-    }
-]
-
-for local_globals in local_globals_list :
-    with open(benchmark_path) as f:
-        code = compile(f.read(), benchmark_path, "exec")
-        globals().update(local_globals)
-        globals()["__file__"] = benchmark_path
-        globals()["exp_N_SAMPLES_TRAIN"] = 1000
-        globals()["exp_N_ITERATIONS"] = 10000
-        globals()["exp_IS_MODE_2_ABLATION"] = False
-        globals()["exp_IS_TWOLINKAGE_CONSTRAINED"] = False
-        exec(code, globals())
-
-'''
 '''
 # Experiment: The Activation Function Matters Too
 
